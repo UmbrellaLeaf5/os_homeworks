cf79bb8682c51ec5875bc4bc45453c97e8ebe6a2
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-11-01 01:47:42 +0300

    fix typo

--- lib/lib.h
+++ lib/lib.h
@@ -12,7 +12,7 @@ int Error(const char* format, ...);
 /**
  * @brief Выводит сообщение об ошибке в stderr ошибок и закрывает файлы.
  * @param format: строка формата вывода.
- * @param ... доп. аргументы для формата. и список файлов.
+ * @param ... доп. аргументы для формата и список файлов.
  * @return 1.
  */
 int ErrorWithFiles(const char* format, ...);
