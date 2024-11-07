4ceabf79151b74f0181e710a5bd53595641752d3
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-11-08 01:40:45 +0300

    update lib.h (PushStrToFront, DedupArray, SortArray)

--- lib/lib.c
+++ lib/lib.c
@@ -27,6 +27,41 @@ int ErrorWithFiles(const char* format, ...) {
   for (FILE* file; (file = va_arg(file_args, FILE*));) fclose(file);
 
   va_end(file_args);
-
   return 1;
+}
+
+inline void PushStrToFront(const char* front_str, char* orig_str) {
+  // временный массив, достаточно большой, чтобы вместить обе строки
+  char temp[strlen(front_str) + strlen(orig_str) + 1];
+
+  strcpy(temp, front_str);
+  strcpy(temp + strlen(front_str), orig_str);
+  strcpy(orig_str, temp);
+}
+
+int DedupArray(int* arr, int arr_size) {
+  if (arr_size <= 1) return arr_size;  // если пуст
+
+  int j = 0;
+  for (int i = 0; i < arr_size; i++) {
+    int is_duplicate = 0;
+
+    // встречается ли текущий элемент arr[i] в массиве после него
+    for (int k = i + 1; k < arr_size; k++)
+      if (arr[i] == arr[k]) {
+        is_duplicate = 1;
+        break;
+      }
+
+    // элемент не является дубликатом, добавляем его в новый массив
+    if (!is_duplicate) arr[j++] = arr[i];
+  }
+
+  return j;
+}
+
+int CompareInts(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+
+void SortArray(int* arr, int arr_size) {
+  qsort(arr, arr_size, sizeof(int), CompareInts);
 }
--- lib/lib.h
+++ lib/lib.h
@@ -1,5 +1,10 @@
+#pragma once
+
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * @brief Выводит сообщение об ошибке в stderr.
@@ -7,7 +12,7 @@
  * @param ... доп. аргументы для формата.
  * @return 1.
  */
-int Error(const char* format, ...);
+int Error(const char *format, ...);
 
 /**
  * @brief Выводит сообщение об ошибке в stderr ошибок и закрывает файлы.
@@ -15,4 +20,26 @@ int Error(const char* format, ...);
  * @param ... доп. аргументы для формата и список файлов.
  * @return 1.
  */
-int ErrorWithFiles(const char* format, ...);
+int ErrorWithFiles(const char *format, ...);
+
+/**
+ * @brief Добавляет строку в начало другой строки.
+ * @param front_str: строка, которую нужно добавить в начало.
+ * @param orig_str: строка, к которой нужно добавить `front_str`.
+ */
+void PushStrToFront(const char *front_str, char *orig_str);
+
+/**
+ * @brief Удаляет дубликаты из массива целых чисел.
+ * @param arr: массив целых чисел.
+ * @param arr_size: размер массива.
+ * @return int: количество уникальных элементов в массиве.
+ */
+int DedupArray(int *arr, int arr_size);
+
+/**
+ * @brief Сортирует массив целых чисел по возрастанию.
+ * @param arr: массив целых чисел.
+ * @param arr_size: размер массива.
+ */
+void SortArray(int *arr, int arr_size);
